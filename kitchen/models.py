2a954769d59f51baa4ab453ae0c91cc02bd0ec83
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-04-03 02:53:36 +0300

    created models in model.py added authorized model to settings registered models in admin.py

--- kitchen/models.py
+++ kitchen/models.py
@@ -1,3 +1,32 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+
+class DishType(models.Model):
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
+
+
+class Cook(AbstractUser):
+    years_of_experience = models.IntegerField(default=0)
+
+    def __str__(self):
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Dish(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    dish_type = models.ForeignKey(DishType, on_delete=models.CASCADE, related_name="dishes")
+    cooks = models.ManyToManyField(Cook, related_name="assigned_dishes")
+
+    class Meta:
+        ordering = ("dish_type__name", "name")
+        verbose_name = "dish"
+        verbose_name_plural = "dishes"
+
+    def __str__(self):
+        return f"{self.name}: ${self.price}"
