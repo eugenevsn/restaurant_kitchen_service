944efd920702d375ca757060d7aa82e5fe1d7975
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-04-03 19:04:35 +0300

    Added routes to the root urls.py. Created the urls.py file in the application folder. Included routing from the application using the namespace "kitchen".

--- kitchen/views.py
+++ kitchen/views.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
-# Create your views here.
+
+@login_required
+def index(request):
+    """View function for home page"""
+
+    num_visits = request.session.get("num_visits", 0)
+    request.session["num_visits"] = num_visits + 1
+
+    context = {
+        "num_visits": num_visits + 1,
+    }
+
+    return render(request, "kitchen/index.html", context=context)
