6dcc92a02eef32f7c68e7245bd319b850074d70d
Evgeniy Vasin <evgeniy97v@gmail.com>
2024-04-05 13:43:21 +0300

    crispy-bootstrap4 allowed in settings.py

--- restaurant_kitchen_service/settings.py
+++ restaurant_kitchen_service/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "kitchen",
+    "crispy_forms",
+    "crispy_bootstrap4",
 ]
 
 MIDDLEWARE = [
@@ -129,3 +131,5 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CRISPY_TEMPLATE_PACK = "bootstrap4"
